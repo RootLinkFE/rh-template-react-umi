6c76ec853b30638772b60a2661894c91c516627e
giscafer <giscafer@outlook.com>
2021-06-24 14:36:07 +0800

    chore: 修改字段

--- src/pages/protocol-manage/index.tsx
+++ src/pages/protocol-manage/index.tsx
@@ -1,38 +1,55 @@
-import React, { useRef } from 'react';
-import { PlusOutlined, EllipsisOutlined } from '@ant-design/icons';
-import { Button, Tag, Space, Menu, Dropdown } from 'antd';
-import type { ProColumns, ActionType } from '@ant-design/pro-table';
+import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import type { ActionType, ProColumns } from '@ant-design/pro-table';
 import ProTable, { TableDropdown } from '@ant-design/pro-table';
-import request from 'umi-request';
+import { Button, Dropdown, Menu } from 'antd';
+import React, { useRef } from 'react';
 
-type GithubIssueItem = {
-  url: string;
-  id: number;
-  number: number;
-  title: string;
-  labels: {
-    name: string;
-    color: string;
-  }[];
-  state: string;
-  comments: number;
-  created_at: string;
-  updated_at: string;
-  closed_at?: string;
+export type TableListItem = {
+  id?: number | string | undefined;
+  key: number;
+  name: string;
+  containers: number;
+  creator: string;
+  status?: string;
+  createdAt: number;
+  updatedAt: number;
+  description: string;
+  picture: string;
+  type: string;
 };
+const tableListDataSource: TableListItem[] = [];
 
-const columns: ProColumns<GithubIssueItem>[] = [
+const creators = ['付小小', '曲丽丽', '林东东', '陈帅帅', '兼某某'];
+const types = ['挖掘机', '起重机', '泵机', '升降机'];
+
+for (let i = 0; i < 20; i += 1) {
+  tableListDataSource.push({
+    key: i,
+    name: '汽车起重机',
+    picture:
+      'https://d1icd6shlvmxi6.cloudfront.net/gsc/ASLBQK/38/1c/96/381c96a7d4e64aa685b5dae8d037d028/images/%E8%BF%9E%E6%8E%A5%E5%BB%BA%E6%A8%A1/u60.png?token=f686e99e7946b4b5f004e474d754e990bffe161515e458dbb22ef46f5d8e9b13',
+    containers: Math.floor(Math.random() * 20),
+    creator: creators[Math.floor(Math.random() * creators.length)],
+    createdAt: Date.now() - Math.floor(Math.random() * 2000),
+    updatedAt: Date.now() - Math.floor(Math.random() * 2000),
+    type: types[Math.floor(Math.random() * creators.length)],
+    description:
+      i % 2 === 1
+        ? '很长很长很长很长很长很长很长的文字要展示但是要留下尾巴'
+        : '简短备注文案',
+  });
+}
+
+const columns: ProColumns<TableListItem>[] = [
   {
     dataIndex: 'index',
     valueType: 'indexBorder',
     width: 48,
   },
   {
-    title: '标题',
-    dataIndex: 'title',
-    copyable: true,
+    title: '模型名称',
+    dataIndex: 'name',
     ellipsis: true,
-    tip: '标题过长会自动收缩',
     formItemProps: {
       rules: [
         {
@@ -43,71 +60,49 @@ const columns: ProColumns<GithubIssueItem>[] = [
     },
   },
   {
-    title: '状态',
-    dataIndex: 'state',
-    filters: true,
-    onFilter: true,
-    valueType: 'select',
-    valueEnum: {
-      all: { text: '全部', status: 'Default' },
-      open: {
-        text: '未解决',
-        status: 'Error',
-      },
-      closed: {
-        text: '已解决',
-        status: 'Success',
-        disabled: true,
-      },
-      processing: {
-        text: '解决中',
-        status: 'Processing',
-      },
-    },
+    title: '创建人',
+    dataIndex: 'creator',
+    ellipsis: true,
   },
   {
-    title: '标签',
-    dataIndex: 'labels',
+    title: '模型描述',
+    dataIndex: 'description',
+    ellipsis: true,
     search: false,
-    renderFormItem: (_, { defaultRender }) => {
-      return defaultRender(_);
+  },
+
+  {
+    title: '模型分类',
+    dataIndex: 'type',
+    initialValue: 'all',
+    filters: true,
+    onFilter: true,
+    valueEnum: {
+      all: { text: '全部', status: 'all' },
+      close: { text: '起重机', status: 'Default' },
+      running: { text: '挖掘机', status: 'digger' },
+      online: { text: '泵车', status: 'Success' },
+      error: { text: '升降机', status: 'Error' },
     },
-    render: (_, record) => (
-      <Space>
-        {record.labels.map(({ name, color }) => (
-          <Tag color={color} key={name}>
-            {name}
-          </Tag>
-        ))}
-      </Space>
-    ),
   },
   {
     title: '创建时间',
     key: 'showTime',
-    dataIndex: 'created_at',
+    dataIndex: 'createdAt',
     valueType: 'date',
     sorter: true,
     hideInSearch: true,
   },
   {
-    title: '创建时间',
-    dataIndex: 'created_at',
-    valueType: 'dateRange',
-    hideInTable: true,
-    search: {
-      transform: (value) => {
-        return {
-          startTime: value[0],
-          endTime: value[1],
-        };
-      },
-    },
+    title: '更新时间',
+    dataIndex: 'updatedAt',
+    valueType: 'dateTime',
+    hideInSearch: true,
   },
   {
     title: '操作',
     valueType: 'option',
-    render: (text, record, _, action) => [
+    render: (text, record: any, _, action) => [
       <a
         key="editable"
         onClick={() => {
@@ -142,15 +137,15 @@ const menu = (
 export default () => {
   const actionRef = useRef<ActionType>();
   return (
-    <ProTable<GithubIssueItem>
+    <ProTable<TableListItem>
       columns={columns}
       actionRef={actionRef}
-      request={async (params = {}, sort, filter) => {
-        console.log(sort, filter);
-        return request<{
-          data: GithubIssueItem[];
-        }>('https://proapi.azurewebsites.net/github/issues', {
-          params,
+      request={(params, sorter, filter): Promise<any> => {
+        // 表单搜索项会从 params 传入，传递给后端接口。
+        console.log(params, sorter, filter);
+        return Promise.resolve({
+          data: tableListDataSource,
+          success: true,
         });
       }}
       editable={{
@@ -173,7 +168,7 @@ export default () => {
         },
       }}
       pagination={{
-        pageSize: 5,
+        pageSize: 10,
       }}
       dateFormatter="string"
       headerTitle="模型管理"
